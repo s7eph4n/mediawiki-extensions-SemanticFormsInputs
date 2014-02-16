7ebfe54752b8574792b06b444dda9d521dd9f6d5
Stephan Gambke <s7eph4n@gmail.com>
2014-02-16 03:37:15 +0000

    another style fix
    Change-Id: I5ec68faaf75a7994fd4b2c7091488ec02cd7ee13

--- libs/datecheck.js
+++ libs/datecheck.js
@@ -7,6 +7,7 @@
 
 window.SFI_DateCheck_init = function ( input_id, params ) {
 	$.validate({
-		borderColorOnError : '', // Border color of elements whith invalid value; empty string to not change border color as it messes up the style of the input box
+		borderColorOnError : '', // Border color of elements with invalid value; empty string to not change border color as it messes up the style of the input box
+		errorElementClass : 'form-error'  // Class that will be put on elements with invalid value
 	});
 };
