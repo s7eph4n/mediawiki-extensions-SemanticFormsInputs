1fdf17b3feec40a4ed9c397c917dd6ead3fe4f53
Stephan Gambke <s7eph4n@gmail.com>
2014-03-01 03:15:11 +0000

    Show error message when form is submitted with erroneous input
    Change-Id: I4ccd795ecf71bae1d21f9122377f50b6c05fa039

--- libs/datecheck.js
+++ libs/datecheck.js
@@ -5,9 +5,14 @@
  *
  */
 
-window.SFI_DateCheck_init = function ( input_id, params ) {
-	$.validate({
+window.SFI_DateCheck_init = function(input_id, params) {
+    $.validate({
 		borderColorOnError : '', // Border color of elements with invalid value; empty string to not change border color as it messes up the style of the input box
-		errorElementClass : 'form-error'  // Class that will be put on elements with invalid value
-	});
+		errorElementClass : 'form-error',  // Class that will be put on elements with invalid value
+        onError: function() {
+            if (!document.getElementById("form_error_header")) {
+                jQuery("#contentSub").append('<div id="form_error_header" class="errorbox" style="font-size: medium"><img src="' + mw.config.get('sfgScriptPath') + '/skins/MW-Icon-AlertMark.png" />&nbsp;' + mw.message('sf_formerrors_header').escaped() + '</div>');
+            }
+        }
+    });
 };
